5ab71067deb4df2cd8140e983397d113bef4d8f9
Michael Fisher <michael.fisher.qaengineer@gmail.com>
2026-02-19 17:41:22 -0800

    make login data-driven

--- tests/TC02_Login.spec.js
+++ tests/TC02_Login.spec.js
@@ -2,19 +2,21 @@ const {test, expect, chromium} = require('@playwright/test');
 const { HomePage } = require('../pages/HomePage');
 const { LoginPage } = require('../pages/LoginPage');
 const { AccountPage } = require('../pages/AccountPage')
-// const { AccountPage } = require('../pages/AccountPage');
+const dataset = JSON.parse(JSON.stringify(require('../tests/test-data/CloydBerryStoreTestData.json')));
+const users = dataset.Sheet1;
 
-test('@sanity @datadriven @regression TC02_Login', async ({page}) => {
-    const home = new HomePage(page);
-    const login = new LoginPage(page);
-    const account = new AccountPage(page);
-    await home.goto();
-    // await page.pause();
-    await home.clickMyAccount();
-    await home.clickLogin();
+for(const data of users) {
+    test.only(`@sanity @datadriven @regression TC02_Login-${data.username}`, async ({page}) => {
+        const home = new HomePage(page);
+        const login = new LoginPage(page);
+        const account = new AccountPage(page);
+        await home.goto();
+        await home.clickMyAccount();
+        await home.clickLogin();
 
-    await login.login("michael.fisher.qaengineer@gmail.com", "123321");
-    await console.log(account.getMyAccountLabel());
-    await expect(account.getMyAccountLabel()).toContainText("My Account");
+        await login.login("michael.fisher.qaengineer@gmail.com", "123321");
+        await console.log(account.getMyAccountLabel());
+        await expect(account.getMyAccountLabel()).toContainText("My Account");
 
-});
+    });
+}
